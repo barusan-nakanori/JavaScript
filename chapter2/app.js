3bedf28e843678e38ce1ea03529a845b9964dbf5
中村倫士 <nori@nmbp.local>
2020-05-21 17:36:08 +0900

    [Finish]6-2

--- chapter2/app.js
+++ chapter2/app.js
@@ -1,1 +1,52 @@
-alert("HelloWorld");
+// var hello = "HelloWorld";
+// alert(hello);
+
+// var int1 = "1";
+// alert(int1);
+
+// alert(65 % 8);
+
+// var str1 = "Hello";
+// var str2 = "World";
+// alert(str1 + str2);
+
+// var orange = 100;
+// var apple = 120;
+
+// if(orange > apple){
+// 	alert("オレンジが高い");
+// }
+// else if(orange == apple){
+// 	alert("同じ値段");
+// }
+// else{
+// 	alert("リンゴが高い");
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+// 	num = num * 2;
+// 	count = count + 1;
+// }
+
+// alert("2をかけ続けて"　+ max + "を超えるのは"　+ count + "回目です。");
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+// 	num = num +i;
+// }
+
+// alert("1から10まで足し算した結果は" + num + "です。");
+
+var int1 = 10;
+var int2 = 5;
+
+alert(int1 + int2);
+alert(int1 - int2);
+alert(int1 * int2);
+alert(int1 / int2);
