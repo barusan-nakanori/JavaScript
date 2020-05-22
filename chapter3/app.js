c6df08dfde076e889870785aa18a26d39fcb4abf
中村倫士 <nori@nmbp.local>
2020-05-22 10:11:20 +0900

    [Finish]6-3

--- chapter3/app.js
+++ chapter3/app.js
@@ -12,13 +12,21 @@
 
 // alert(promptStr);
 
-var user_hand = prompt("グー、チョキ、パーのどれかを選んでね");
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+    while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+      alert('グー・チョキ・パーのいずれかを入力して下さい');
+      user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+    }
 
 var js_hand = getJShand();
 
 var judge = winLose(user_hand, js_hand);
 
-alert("あなたは"　+ user_hand　+ "を出しました。\n相手は" + js_hand +"を出しました。\n勝敗は" + judge +"です。");
+if (user_hand != null) {
+        alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+    } else {
+        alert("またチャレンジしてね")
+    }
 
 function getJShand(){
 	var js_hand_num = Math.floor( Math.random()*3);
@@ -31,6 +39,7 @@ function getJShand(){
 	}else if(js_hand_num　= 2){
 		hand_name = "パー";
 	}
+	
 
 	return hand_name;
 }
